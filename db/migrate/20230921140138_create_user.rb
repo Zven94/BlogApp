926deeb3b1b123d4fbf7ff41ba799f88a478f49e
Zven94 <nicobolanez@gmail.com>
2023-09-21 17:44:54 -0300

    fix tables foreign keys

--- db/migrate/20230921140138_create_user.rb
+++ db/migrate/20230921140138_create_user.rb
@@ -2,9 +2,9 @@ class CreateUser < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :photo_url
+      t.string :photo
       t.string :bio
-      t.integer :post_counter, default : 0
+      t.integer :post_counter
       t.timestamps
     end
   end
