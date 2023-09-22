0cff0d47af38cd647e1180a72b99f825eea73e40
Zven94 <nicobolanez@gmail.com>
2023-09-21 23:21:22 -0300

    fix rubocop issues

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
-  
+
   def update_user_comment_counter
     author.update(comment_counter: author.comments.count)
   end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,7 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  
+
   def update_post_like_counter
     post.update(likes_counter: post.likes.count)
   end
