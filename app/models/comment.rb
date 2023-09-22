a08c133543f760aa267abdc7d8ab97a160be1a14
Zven94 <nicobolanez@gmail.com>
2023-09-21 23:09:56 -0300

    add custom method for classes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :post
-  belongs_to :user
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  
+  def update_user_comment_counter
+    author.update(comment_counter: author.comments.count)
+  end
 end
