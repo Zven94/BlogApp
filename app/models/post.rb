f36d73d23a40e40af02fabdda2cc80cb56889c42
Zven94 <nicobolanez@gmail.com>
2023-09-22 21:02:52 -0300

    add spec tests for User & Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,22 +1,22 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_many :posts, foreign_key: 'author_id'
   has_many :comments
   has_many :likes
 
-  # validations
-
-  validates :title, presence: true
-  validates :title, length: { maximum: 250 }
+  # Validations
+  validates :title, presence: true, length: { maximum: 250 }
   validates :comment_counter, numericality: { greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
-  
-  # methods
+
+  # Callback to update user's post_counter
+  after_create :update_user_post_counter
+
+  # Methods
   def update_user_post_counter
     author.update(post_counter: author.posts.count)
   end
 
-  def return_most_recent_comment
-    comments.order(created_at: :desc).limit(5)
+  def return_most_recent_comments(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'validates presence of name' do
+    user = User.new(post_counter: 0)
+    expect(user).to_not be_valid
+    expect(user.errors[:name]).to include("can't be blank")
+  end
+  it 'validates the post counter must be a positive number' do
+    user = User.new(post_counter: -5)
+    expect(user).to_not be_valid
+    expect(user.errors[:post_counter]).to include("must be greater than or equal to 0")
+  end
+  it 'validates the recent post method' do
+    user = User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+    user.save
+    recent_posts = user.recent_posts
+    expect(recent_posts.count).to eq(0)
+  end
 end
