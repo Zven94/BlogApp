a22f1f5f68d0660f7d6e51ad28a91492e9184665
Zven94 <nicobolanez@gmail.com>
2023-09-26 16:05:29 -0300

    created routes for models: users, posts, comments, likes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :users, only: [:index, :new, :udpdate, :show] do
+    resources :posts, only: [:index, :new, :show] do
+      resources :comments, only: [:index, :new, :show] 
+      resources :likes, only: [:index, :update]
+    end
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
