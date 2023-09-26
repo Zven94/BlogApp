2651900c39b0151d1891c81b9c630f85cced475f
Zven94 <nicobolanez@gmail.com>
2023-09-26 19:28:04 -0300

    fix actions for controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index, :new, :create, :udpdate, :show] do
-    resources :posts, only: [:index, :new, :create, :show] do
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show] do
       resources :comments, only: [:index, :new, :create, :show] 
       resources :likes, only: [:create, :destroy]
     end
