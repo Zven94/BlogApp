960658b3c8ec7b87326e12e150aa321e7f398e99
Zven94 <nicobolanez@gmail.com>
2023-09-29 13:53:19 -0300

    added recent comments on Post Index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,26 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @user = User.find(params[:user_id])
+    @post = Post.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = @user.posts.build(post_params)
+
+    if @post.save
+      redirect_to user_post_path(@user, @post), notice: 'Post created successfully.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:index, :new, :create, :show] 
       resources :likes, only: [:create, :destroy]
     end
   end
-  # Defines the root path route ("/")
 end
