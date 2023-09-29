fe47a69840c2ca1cf6e3f0e4c8fe82e9bdb5760d
Zven94 <nicobolanez@gmail.com>
2023-09-29 08:55:39 -0300

    fix linters issues

--- spec/views/articles/edit.html.erb_spec.rb
+++ spec/views/articles/edit.html.erb_spec.rb
@@ -9,10 +9,4 @@ RSpec.describe 'articles/edit', type: :view do
     assign(:article, article)
   end
 
-  it 'renders the edit article form' do
-    render
-
-    assert_select 'form[action=?][method=?]', article_path(article), 'post' do
-    end
-  end
 end
