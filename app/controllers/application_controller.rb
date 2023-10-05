3a5c7dab8756cc11c2c176e8250f8786f6c895fa
Zven94 <nicobolanez@gmail.com>
2023-10-05 16:50:58 -0300

    deleted commented code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,6 @@ class ApplicationController < ActionController::Base
 
   private
 
-  # def current_user
-  #   @current_user = User.first
-  # end
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
